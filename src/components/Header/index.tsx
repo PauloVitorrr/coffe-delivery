331daa61a6ee6d52d258038c49a17d1e80afff29
Paulo Vitor <paulovitorvaztst@gmail.com>
2024-12-03 11:14:13 -0300

    feat: :white_check_mark: Header e responsividade do header concluido

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,21 @@
 import * as S from "./styles";
 
+import logo from "../../assets/Logo.svg";
+import { MapPin, ShoppingCart } from "phosphor-react";
+
 export function Header() {
   return (
     <S.HeaderContainer>
-      <h1>teste</h1>
+      <img src={logo} alt="Logo café" />
+      <S.ContainerCart>
+        <S.ContentLocation>
+          <MapPin size={16} color="#8047F8" weight="fill" />
+          Fortaleza, CE
+        </S.ContentLocation>
+        <S.ContentCart>
+          <ShoppingCart size={24} color="#C47F17" weight="fill" />
+        </S.ContentCart>
+      </S.ContainerCart>
     </S.HeaderContainer>
   );
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -2,4 +2,38 @@ import styled from "styled-components";
 
 export const HeaderContainer = styled.header`
   background-color: ${(props) => props.theme["background"]};
+  padding: 32px 160px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  @media (max-width: 900px) {
+    padding: 16px 80px;
+  }
+  @media (max-width: 589px) {
+    padding: 16px;
+  }
+`;
+
+export const ContainerCart = styled.div`
+  display: flex;
+  gap: 12px;
+`;
+
+export const ContentLocation = styled.p`
+  display: flex;
+  align-items: center;
+  gap: 2px;
+  padding: 6px;
+  background-color: ${(props) => props.theme["purple-light"]};
+  border-radius: 4px;
+  font-family: "Roboto", sans-serif;
+  font-size: 14px;
+  color: ${(props) => props.theme["purple-dark"]};
+`;
+
+export const ContentCart = styled.div`
+  padding: 6px;
+  background-color: ${(props) => props.theme["yellow-light"]};
+  border-radius: 4px;
 `;
